b01e270900ee7c1ab66462e139fb6af6c757899c
turingvsclarke <turingvsclarke@turingvsclarkes-MacBook-Air.local>
2024-07-30 16:12:20 -0400

    Added first few challenges

--- stockPriceChecker/routes/api.js
+++ stockPriceChecker/routes/api.js
@@ -1,10 +1,13 @@
 'use strict';
+const helmet=require('helmet')
 
 module.exports = function (app) {
+  app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self'"],scriptSrc:["'self'"],styleSrc:["'self'"]}}))
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      let stockString=String(req.query.stock)
+      console.log(req.query)
+      res.json({stockData:{stock:stockString,price:Number(req.query.price),likes:Number(req.query.likes)}})
     });
-    
 };
