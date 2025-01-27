b2051fe25a63cbf2eea9ba44bc125ce196076504
turingvsclarke Eades <rhinoeades@gmail.com>
2025-01-27 01:45:37 +0000

    fix: Added mongodb commands

--- anonymousMessageBoard/database.js
+++ anonymousMessageBoard/database.js
@@ -1,87 +1,150 @@
-const mysql=require("mysql2");
-require('dotenv').config();
+// const mysql=require("mysql2");
+import dotenv from 'dotenv';
+dotenv.config();
 
-const pool=mysql.createPool({
-    host: '127.0.0.1',
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
+import {MongoClient,ObjectId} from 'mongodb';
 
-const row=async ()=>await pool.query("insert into boards(board_name) values(?)",["Gamers"]);
-row().then(x=>console.log(x));
+var url = process.env.MONGO_URI;
 
-module.exports={
-    reportReply: async function(_id){
-        const result=await pool.query("update replies set reported=true where _id=?",[_id])
-        return result
-    },
-    reportThread: async function(_id){
-        const result=await pool.query("update threads set reported=true where _id=?",[_id])
-        return result
-    },
-    deleteReply: async function(_id,delete_password){
-        const [row]=await pool.query("select * from replies where _id=? and delete_password=?",[_id,delete_password])
-        if(row.length>0){
-            // Mark that thread as deleted
-            const result=await pool.query("update replies set reply_text='[deleted]' where _id=?",[_id])
-            // Mark all associated replies as deleted
-            const result1=await pool.query("update replies set deleted=true where _id=?",[_id])
-            // set the deleted column of all the replies with that _id to true
-            return true;
-        }
-        return false;
-    },
-    deleteThread: async function(_id,delete_password){
-        const [row]=await pool.query("select * from threads where _id=? and delete_password=?",[_id,delete_password])
-        if(row.length>0){
-            // Mark that thread as deleted
-            const result=await pool.query("update threads set deleted=1 where _id=?",[_id])
-            // Mark all associated replies as deleted
-            const result1=await pool.query("update replies set deleted=1 where _id=?",[_id])
-            // set the deleted column of all the replies with that _id to true
-            return true;
-        }
-        return false;
-    },
-    insertReply: async function(text, delete_password, _id){
-        await pool.query("insert into replies(reply_text, delete_password, _id) values(?,?,?)",[text,delete_password,_id])
-        const [result]=await pool.query("SELECT * FROM replies WHERE _id=LAST_INSERT_ID()");
-        const result1=await pool.query("update threads set bumped_on=? where _id=?",[result[0].created_on,_id])
-        
-        return result._id
-    },
+const client=new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+client.connect();
+//await listDatabases(client);
 
-    insertThread: async function(text,delete_password,board_name){
-        // Get the board id
-        var [board]=await pool.query("select * from boards where board_name=?",[board_name])
-        // Add the board if it doesn't already exist
-        if(board.length==0){
-            await pool.query("insert into boards(board_name) values(?)",[board_name]);
-            [board] = await pool.query("select * from boards where board_name=?",[board_name])
+const db=client.db('MessageBoard')
+
+await db.admin({listDatabases:1});
+
+const testThreads=db.collection('fcc_test');
+
+const thread=await testThreads.findOne({text:'fcc_test_thread'});
+console.log(thread.replies);
+
+const report=await reportReply('fcc_test','66e8a3056f9fb2ecc728c5b6',0);
+console.log(report);
+
+export async function reportReply(board_name,thread_id,reply_id){
+        // Replies is an array within the thread object. We want to grab the replies array,
+        // change the reported value to true, then update that thread with the correct id.
+        // Step 1: Grab the thread
+        thread_id=new ObjectId(thread_id);
+        const thread=await db.collection(board_name).findOne({_id:thread_id});
+        // Update the repy so that reported becomes true
+        var replies=[...thread.replies];
+        replies.map(reply=>{
+            if(reply._id==reply_id){
+                reply.reported=true;
+            }
+            return reply;
+
+            });
+        // Update the thread to have the new replies array as its replies value
+        db.collection(board_name).updateOne({_id:thread_id},
+            { $set: {replies: replies},
+            $currentDate: {bumped_on:{$type:"timestamp"}}}
+        );
+        return "reported";
+}
+
+
+
+export async function reportThread(board_name,thread_id){
+        thread_id=new ObjectId(thread_id);
+        db.collection(board_name).updateOne({_id:thread_id},
+            { $set: {reported: true},
+            $currentDate: {bumped_on:{$type:"timestamp"}}}
+        );
+        return 'reported';
+    }
+
+export async function deleteReply(board_name,thread_id,reply_id,delete_password){
+        // Find the 
+        thread_id=new ObjectId(thread_id);
+        const thread=await db.collection(board_name).findOne({_id:thread_id});
+        var replies=[...thread.replies];
+        var replyCopy=replies.filter(reply=>reply._id==reply_id)[0];
+        if(replyCopy.delete_password==delete_password){
+            replyCopy.text='[deleted]';
+            db.collection(board_name).updateOne({_id:thread_id},
+                { $set: {replies: replies},
+                $currentDate: {bumped_on:{$type:"timestamp"}}}
+            );
+            return 'success';
+        }else{
+            return 'incorrect password';
         }
-        let board_id=board[0].board_id
-        const result=await pool.query("insert into threads(thread_text,delete_password,board_id) values(?,?,?)",[text,delete_password,board_id])
-        return result._id
-    },
-    getThreadsFromBoard: async function(board_name){
-        const [board_id]=await pool.query("select board_id from boards where board_name=?",[board_name])
-        var [threads]=await pool.query("select _id, thread_text, created_on, bumped_on from threads where board_id=? and not deleted order by bumped_on",board_id[0])
-        threads=threads.slice(0,)
-        for (var thread of threads){
-            var [replies]=await pool.query("select _id, reply_text, created_on from replies where _id=? and not deleted order by created_on",[thread._id])
-            replies=replies.slice(0,3);
-            thread.replies=replies;
+    }
+
+export async function deleteThread(board_name,thread_id,delete_password){
+        thread_id=new ObjectId(thread_id);
+        const thread=await db.collection(board_name).findOne({_id:thread_id});
+        if(thread.delete_password==delete_password){
+            // delete the thread from the database
+            db.collection(board_name).deleteOne({_id:thread_id});
+            return 'success';
+        }else{
+            return 'incorrect password';
         }
-        return threads;
-    },
-    getThread: async function getThread(_id){
-        const [threads]=await pool.query("select _id, thread_text, created_on, bumped_on from threads where _id=? and not deleted",_id)
-        const [replies]=await pool.query("select _id, reply_text, created_on from replies where _id=? and not deleted order by created_on",[_id])
-        let thread=threads[0];
-        if(thread){
-            thread.replies=replies;
-        }
+    }
+
+export async function insertReply(board_name,thread_id,text,delete_password){
+    thread_id=new ObjectId(thread_id);
+    var thread=await db.collection(board_name).findOne({_id:thread_id});
+    let replies=[...thread.replies];
+    let replyLength=replies.length;
+    replies.push({
+        _id: replyLength,
+        text: text,
+        created_on: Date.now(),
+        delete_password: delete_password,
+        reported: false
+    });
+    // Update the thread
+    db.collection(board_name).updateOne({_id:thread_id},
+        { $set: {replies: replies,bumped_on:Date.now()}}
+    );
+
+    }
+
+export async function insertThread(board_name,text,delete_password){
+        await db.collection(board_name).insertOne({
+            text: text,
+            created_on: Date.now(),
+            bumped_on: Date.now(),
+            reported: false,
+            delete_password: delete_password,
+            replies: []
+        })
+    }
+
+export async function getThread(board_name,thread_id){
+        thread_id=new ObjectId(thread_id);
+        var thread=await db.collection(board_name).findOne({_id:thread_id});
+        thread={...thread};
+        delete thread.reported;
+        delete thread.delete_password;
+        thread.replies.forEach(reply=>{
+            delete reply.delete_password;
+            delete reply.reported;
+        })
         return thread;
     }
-}
+
+export async function getThreadsFromBoard(board_name){
+        // Grab all the threads. Only take the first ten entries of that array. 
+        // For each thread, grab the first 
+        var threads=await db.collection(board_name).find();
+        threads=[...threads];
+        threads.sort((thread1,thread2)=>thread1.bumped_on-thread2.bumped_on);
+        threads=threads.slice(0,10);
+        threads.forEach(thread=>{
+            delete thread.reported;
+            delete thread.delete_password;
+            let replies=replies.sort((reply1,reply2)=>reply1.created_on-reply2.created_on).slice(0,3);
+            replies.forEach(reply=>{
+                delete reply.delete_password;
+                delete reply.reported;
+            });
+        });
+        return threads;
+    }
+
